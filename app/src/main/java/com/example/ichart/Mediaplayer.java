d93c825675833d619993daf0d9349ac2f188e1c4
xiazi21 <zfx5078@psu.edu>
2021-03-28 16:38:15 +0800

    for media player initial commit

---
+++ app/src/main/java/com/example/ichart/Mediaplayer.java
@@ -0,0 +1,14 @@
+package com.example.ichart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mediaplayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mediaplayer);
+    }
+}
